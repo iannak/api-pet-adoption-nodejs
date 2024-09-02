660165273c0861bb8b64b8fdc77b5e2ac42455e9
iannak <franz.karols@gmail.com>
2024-09-01 22:45:42 -0300

    (fix): change nomenclature pt to en

--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -1,16 +1,17 @@
 import express from "express";
+import { AppDataSource } from "../config/dataSource";
 import PetController from "../controller/PetController";
 import PetRepository from "../repositories/PetRepository";
-import { AppDataSource } from "../config/dataSource";
 const router = express.Router();
+
 const petRepository = new PetRepository(
   AppDataSource.getRepository("PetEntity")
 );
 const petController = new PetController(petRepository);
 
-router.post("/", (req, res) => petController.criaPet(req, res));
-router.get("/", (req, res) => petController.listaPet(req, res));
-router.put("/:id", (req, res) => petController.atualizaPet(req, res));
-router.delete("/:id", (req, res) => petController.deletaPet(req, res));
+router.post("/", (req, res) => petController.createPet(req, res));
+router.get("/", (req, res) => petController.listPet(req, res));
+router.put("/:id", (req, res) => petController.updatePet(req, res));
+router.delete("/:id", (req, res) => petController.deletePet(req, res));
 
 export default router;
